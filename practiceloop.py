559cad43dd890ac8e2f186787edc016f39fc08b9
Rohit Pal <rohitpal0194@gmail.com>
2026-06-22 13:57:23 +0530

    new program

--- practiceloop.py
+++ practiceloop.py
@@ -4,9 +4,38 @@
   #  print(i)
 
 # wap to calculate sum of all number 1 to 10
-sum=0
-for i in range (1,11):
-    sum+=i
-print(sum)
+
+# sum=0
+# for i in range (1,11):
+#     sum+=i
+# print(sum)
+
+# wap to check even number from 1 to 10
+
+# for i in range(1,11):
+#     if i%2==0:
+#         print(i,"is even number")
+
+# wap to check all odd number from 1 to 10.
+# for i in range (1,11):
+#     if i%2!=0:
+#         print(i,"is odd number")
+
+# wap to sum of all even number from 1 to 10 .
+# sum_even=0
+# for i in range(1,11):
+#     if i%2==0:
+#         sum_even+=i
+# print(sum_even)
+
+# wap to sum of all odd number from 1 to 10 .
+sum_odd=0
+for i in range(1,11):
+    if i%2!=0:
+        sum_odd+=i
+print(sum_odd)
+
+
+
 
   
