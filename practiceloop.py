74b88220a9381a7f135a63e260a541afbaf2a366
Rohit Pal <rohitpal0194@gmail.com>
2026-06-22 13:33:22 +0530

    new program

--- practiceloop.py
+++ practiceloop.py
@@ -1,4 +1,12 @@
 # wap to print number using for loop 1 to 10.
 
-for i in range(1,10):
-    print(i)
+#for i in range(1,10):
+  #  print(i)
+
+# wap to calculate sum of all number 1 to 10
+sum=0
+for i in range (1,11):
+    sum+=i
+print(sum)
+
+  
